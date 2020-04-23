889db78f44bba2df2ef6dc1b82a2fa8193eb15d9
niranaj1016 <niranjan1016@gmai.com>
2020-04-22 23:35:59 -0400

    Updated tests

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-app.listen(8080, function() {
-    console.log('I am running')
-})
+module.exports = app;
